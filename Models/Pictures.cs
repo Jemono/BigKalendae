234a5ffbb495e2cc5acbbb88a8a14a6e16fcc42b
Jemono <jlamont@AKL-PC-IT5.Auctions.co.nz>
2015-09-24 15:29:13 +1200

    Added Pictures model
    Only a test can be deleted.

---
+++ Models/Pictures.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigKalendae.Models
+{
+    public class Pictures
+    {
+        public string Image { get; set; }
+    }
+}
